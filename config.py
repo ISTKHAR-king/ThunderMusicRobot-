a26cfe6baaef26091540885a42539204f6929f68
ISTKHAR-king <istkharalam225@gmail.com>
2025-06-13 23:08:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,6 +39,9 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
+API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
+API_KEY = getenv("API_KEY", 'NxGBNexGenBots7ed2dc') # youtube song api ke
+
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ll_aizan_star_vibe_ll")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+cnn58zjw8rM4YzY1")
 
